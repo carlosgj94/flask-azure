7f19b7782976391afcb54fdeab898d42012d5812
Max Thayer <garbados@gmail.com>
2013-09-24 18:05:11 -0400

    fleshed out subscriptions

--- resourceprovider/routes/subscriptions.py
+++ resourceprovider/routes/subscriptions.py
@@ -1,6 +1,7 @@
 import flask
 import flask.views
 from flask import request
+from resourceprovider.util import xml_dict
 from resourceprovider.controllers import subscriptions as ctrl
 
 subscriptions = flask.Blueprint('subscriptions', __name__)
@@ -10,4 +11,15 @@ def subscribe(subscription_id):
     """
     handle subscription events: Registered, Disabled, Enabled, Deleted
     """
-    raise NotImplementedError
+    body = xml_dict(request.data)
+    event = body['EntityState'] or body['EntityEvent']
+    if event == 'Registered':
+        ctrl.registered(subscription_id, body)
+    elif event == 'Disabled':
+        ctrl.disabled(subscription_id, body)
+    elif event == 'Enabled':
+        ctrl.enabled(subscription_id, body)
+    elif event == 'Deleted':
+        ctrl.deleted(subscription_id, body)
+    else:
+        flask.abort(400)
