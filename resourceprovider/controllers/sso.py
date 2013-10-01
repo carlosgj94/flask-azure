2a7686b6c25a45736f7ce9261d15fd644b7a7472
Max Thayer <garbados@gmail.com>
2013-10-01 10:44:59 -0400

    sso now passes tests

--- resourceprovider/controllers/sso.py
+++ resourceprovider/controllers/sso.py
@@ -1,17 +1,33 @@
 """
 TODO: what are the sso controllers?
 """
+import hashlib
+import flask
+from datetime import datetime
+import iso8601
 
+def create(subscription_id, cloud_service_name, resource_type, resource_name):
+    """
+    Generates an SSO token and a timestamp indicating its freshness.
+    """
+    sso_secret = flask.current_app.config['SECRET_KEY']
+    signature = "%s:%s:%s:%s:%s" % (subscription_id, cloud_service_name, resource_type, resource_name, sso_secret)
+    return hashlib.sha256(signature).hexdigest(), datetime.now()
 
-def create():
+def get(subscription_id, cloud_service_name, resource_type, resource_name, token, timestamp):
     """
-    This happens when a user logs in to your service for the first time.
+    Return a boolean indicating whether the given token is still valid, by two measures:
+    1. Does it match our SSO secret?
+    2. Is it younger than ten minutes?
     """
-    raise NotImplementedError
-
-
-def get():
-    """
-    This happens when a user logs in to your service after the first time.
-    """
-    raise NotImplementedError
+    local_token, local_timestamp = create(subscription_id, cloud_service_name, resource_type, resource_name)
+    # do the tokens match?
+    if local_token == token:
+        remote_timestamp = iso8601.parse_date(timestamp).replace(tzinfo=None)
+        # is the token recent enough?
+        if (local_timestamp - remote_timestamp).seconds < 60 * 10:
+            return True
+        else:
+            return False
+    else:
+        return False
--- resourceprovider/routes/sso.py
+++ resourceprovider/routes/sso.py
@@ -7,17 +7,23 @@ url = '/subscriptions/<subscription_id>/cloudservices/<cloud_service_name>'
 @sso.route('/subscriptions/<subscription_id>/cloudservices/<cloud_service_name>/resources/<resource_type>/<resource_name>/SsoToken', methods=['POST'])
 def create_sso(subscription_id, cloud_service_name, resource_type, resource_name):
     try:
-        result = ctrl.create(subscription_id, cloud_service_name, resource_type, resource_name)
+        token, timestamp = ctrl.create(subscription_id, cloud_service_name, resource_type, resource_name)
     except NotImplementedError as e:
         return flask.abort(500)
     else:
-        return flask.render_template('sso.xml', **result)
+        return flask.render_template('sso.xml', token=token, timestamp=timestamp)
 
 @sso.route('/sso', methods=['GET'])
 def get_sso():
-    try:
-        result = ctrl.get(**flask.request.args)
-    except NotImplementedError as e:
-        return flask.abort(500)
+    args = dict(
+        subscription_id=flask.request.args['subid'],
+        cloud_service_name=flask.request.args['cloudservicename'],
+        resource_type=flask.request.args['resourcetype'],
+        resource_name=flask.request.args['resourcename'],
+        token=flask.request.args['token'],
+        timestamp=flask.request.args['timestamp']
+        )
+    if ctrl.get(**args):
+        return 'Logged In'
     else:
-        return flask.render_template('sso.xml', **result)
+        return flask.abort(404)
